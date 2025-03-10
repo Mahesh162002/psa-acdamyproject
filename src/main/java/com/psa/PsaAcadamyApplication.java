25ae5d85d1a563b800469db3bfeb4b355246542a
Mahesh <your-email@example.com>
2025-03-10 15:17:12 +0530

    Frist Commit

---
+++ src/main/java/com/psa/PsaAcadamyApplication.java
@@ -0,0 +1,13 @@
+package com.psa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaAcadamyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaAcadamyApplication.class, args);
+	}
+
+}
